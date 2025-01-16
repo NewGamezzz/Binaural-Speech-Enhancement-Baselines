61d78b19642239dc730442b61afead599230efee
Thanapat Trachu <49015055+NewGamezzz@users.noreply.github.com>
2025-01-16 11:29:43 +0700

    edit test.py

--- test.py
+++ test.py
@@ -13,11 +13,10 @@ from src.utils.other import get_lr, si_sdr
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--weight_path", required=True, help="Directory of the weights."
-    )
+    parser.add_argument("--weight_path", required=True, help="Directory of the weights.")
     parser.add_argument("--weight_epoch", required=True, help="An epoch to be loaded.")
     parser.add_argument("--device", default="cuda", help="Inference on cpu or cuda")
+    parser.add_argument("--config", default=None, help="Config path")
 
     args = parser.parse_args()
     return args
@@ -45,18 +44,14 @@ def inference(data_loader, model, device="cuda"):
         target_utterances.append(clean_monaural_utterance)
 
     output_utterances = torch.cat(output_utterances, dim=0).data.cpu().numpy()
-    target_utterances = (
-        torch.cat(target_utterances, dim=0).squeeze(1).data.cpu().numpy()
-    )
+    target_utterances = torch.cat(target_utterances, dim=0).squeeze(1).data.cpu().numpy()
     n_utterance, _ = output.shape
 
     _pesq, _si_sdr, _estoi = 0.0, 0.0, 0.0
     for index in tqdm(range(n_utterance), desc="Calculate Metrics"):
         _si_sdr += si_sdr(target_utterances[index], output_utterances[index])
         _pesq += pesq(16000, target_utterances[index], output_utterances[index], "wb")
-        _estoi += stoi(
-            target_utterances[index], output_utterances[index], 16000, extended=True
-        )
+        _estoi += stoi(target_utterances[index], output_utterances[index], 16000, extended=True)
 
     _si_sdr /= n_utterance
     _pesq /= n_utterance
@@ -68,10 +63,10 @@ if __name__ == "__main__":
     args = get_args()
     print(args)
 
-    state_dict_path = os.path.join(
-        args.weight_path, f"weight/epoch_{args.weight_epoch}.ckpt"
+    state_dict_path = os.path.join(args.weight_path, f"weight/epoch_{args.weight_epoch}.ckpt")
+    config_path = (
+        os.path.join(args.weight_path, ".hydra.yaml") if args.config is None else args.config
     )
-    config_path = os.path.join(args.weight_path, ".hydra.yaml")
 
     state_dict = torch.load(state_dict_path)
     config = load_yaml(config_path)
